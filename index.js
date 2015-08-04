456a42b9484e811646c384fffaafd36b8fb75949
timbur <timothy.burgess.87@gmail.com>
2015-08-03 23:40:18 -0400

    whoops, meant to remove all that

--- index.js
+++ index.js
@@ -5,31 +5,12 @@ module.exports = function (source, map) {
     this.cacheable();
   }
 
-  var code = ''
+  return ''
+    +source+"\n"
+    +"\n"
     +'module.hot.accept(function (err) {'+"\n"
       +'if (err) {'+"\n"
-        +'console.error(err);'+"\n"
+        +'console.error(err);'+"\n"x  
       +'}'+"\n"
     +'});'+"\n";
-
-  if (this.sourceMap === false) {
-    return this.callback(null, ''
-      +source+"\n"
-      +"\n"
-      +code
-    );
-  }
-
-  if (!map) {
-    map = makeIdentitySourceMap(source, this.resourcePath);
-  }
-
-  var node = new SourceNode(null, null, null, ''
-    +SourceNode.fromStringWithSourceMap(source, new SourceMapConsumer(map))
-    +(new SourceNode(null, null, this.resourcePath, code))
-  );
-
-  var result = node.toStringWithSourceMap();
-
-  this.callback(null, result.code, result.map.toString());
 };
